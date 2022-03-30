21f6cbfca8beb2083fea0f739c2ad4cb5894ce2c
jkiang <jkiang@iu.edu>
2022-03-30 15:04:17 -0400

    add interrupt version (not working) of data collect

---
+++ collectBigDataInterrupt.py
@@ -0,0 +1,61 @@
+import pandas as pd
+from requests import get 
+from datetime import datetime
+import tzlocal
+import time
+from tqdm import tqdm
+import signal
+
+system = 'iub'
+folder = 'data/'
+
+def handler(signum, fname):
+    if input("\nexit? y/n: ") == 'y':
+        exit(1)
+
+signal.signal(signal.SIGINT, handler)
+
+def getDF():
+    global system
+    fetch = get(f'http://{system}.doublemap.com/map/v2/buses').json()
+    df = pd.DataFrame(fetch)
+    df.drop(['fields', 'bus_type', 'capacity', 'load'], axis=1, inplace=True)
+    df.sort_values(by=['route'], inplace=True)
+    return df
+
+def stopName(id):
+    stops = pd.DataFrame(get("http://iub.doublemap.com/map/v2/stops").json())
+    try: return(stops.loc[stops['id']==id]['name'].values[0])
+    except: 
+        print("Stop ID Invalid")
+        return 0
+
+def collectBusData(bigDF, interval):
+    for i in tqdm(range(interval)):
+        newDF = getDF()
+        bigDF = pd.concat([newDF, bigDF]).drop_duplicates()
+        time.sleep(1)
+    return bigDF
+
+def saveData(interval):
+    # interval in seconds
+    global folder
+    start = time.strftime("%I:%M:%S%p", time.localtime())
+    
+    stops = pd.DataFrame(get("http://iub.doublemap.com/map/v2/stops").json())
+    stops.drop(['code', 'ivr_code', 'description'], axis=1, inplace=True)
+
+    routes = pd.DataFrame(get("http://iub.doublemap.com/map/v2/routes").json())
+    routes.drop(['description'], axis=1, inplace=True)
+
+    bigDF = pd.DataFrame()
+    bigDF = collectBusData(bigDF, interval)
+    
+    end = time.strftime("%I:%M:%S%p", time.localtime())
+    # fName = start+'_to_'+end+"_on_"+time.strftime("", time.localtime())
+    fName = "testHDF"
+    stops.to_hdf(folder+fName+'.h5', key='stops')
+    routes.to_hdf(folder+fName+'.h5', key='routes')
+    bigDF.to_hdf(folder+fName+'.h5', key='buses')
+
+saveData(2*60*60)
